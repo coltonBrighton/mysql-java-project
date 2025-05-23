4647d4be53b504c9a79ab09fb43c12d8571f742a
Colton <coltonbrighton@gmail.com>
2025-05-22 22:24:35 -0600

    added functionality to add a new project

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -5,17 +5,19 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
+import projects.entity.Project;
 import projects.exception.DbException;
+import projects.service.ProjectService;
 
 public class ProjectsApp {
+	private Scanner scanner = new Scanner(System.in);
+	private ProjectService projectService = new ProjectService();
 	
 	// @formatter:off
 	private List<String> operations = List.of(
 			"1) Add a project"
 	);
 	// @formatter:on
-			
-	private Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -33,7 +35,7 @@ public class ProjectsApp {
 				  case -1: 
 					done = exitMenu();
 					break;
-				  
+				 
 				  case 1:
 					createProject();
 					break;
@@ -50,7 +52,7 @@ public class ProjectsApp {
 
 	private void createProject() {
 		// TODO Auto-generated method stub
-		String projectName = getUserInput("Enter the project name");
+		String projectName = getStringInput("Enter the project name");
 		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours");
 		BigDecimal actualHours = getDecimalInput("Enter the actual hours");
 		Integer difficulty = getIntInput("Enter project difficulty (1-5)");
@@ -63,6 +65,24 @@ public class ProjectsApp {
 		project.setDifficulty(difficulty);
 		project.setNotes(notes);
 		
+		Project dbProject = projectService.addProject(project);
+		System.out.println("You have successfully created project: " + dbProject);
+		
+	}
+
+	private BigDecimal getDecimalInput(String prompt) {
+		// TODO Auto-generated method stub
+		String input = getStringInput(prompt);
+		
+		if (Objects.isNull(input)) {
+			return null;
+		}
+		
+		try {
+			return new BigDecimal(input).setScale(2);
+		} catch (NumberFormatException e) {
+			throw new DbException(input + " is not a valid decimal number.");
+		}
 	}
 
 	private boolean exitMenu() {
